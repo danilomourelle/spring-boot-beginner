fac62d02a319ef0446779ae6755a95ea28acb061
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-24 11:28:04 -0300

    first commit

---
+++ src/test/java/com/danmou/begginer/BegginerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danmou.begginer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BegginerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
