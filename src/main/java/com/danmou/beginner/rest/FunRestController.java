769e6f07ca81eeb04296546e3cb949c2584ae42d
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-25 11:17:07 -0300

    spring-boot-devtools

--- src/main/java/com/danmou/beginner/rest/FunRestController.java
+++ src/main/java/com/danmou/beginner/rest/FunRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
-  @GetMapping(value = "/")
+  @GetMapping(value = "/hello-world")
   public String sayHello() {
     return "Hello Word";
   }
