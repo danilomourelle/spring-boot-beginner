bae0c7dfff2334144eaa6b64f14fa82bf1455777
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-25 13:16:05 -0300

    custom properties at application.properties
    Also disable security dependency to make it easier to run

--- src/main/java/com/danmou/beginner/rest/FunRestController.java
+++ src/main/java/com/danmou/beginner/rest/FunRestController.java
@@ -1,14 +1,22 @@
 package com.danmou.beginner.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+  @Value("${team.coach}")
+  private String teamCoach;
+  @Value("${team.manager}")
+  private String teamManager;
+  @Value("${team.owner}")
+  private String teamOwner;
+
   @GetMapping(value = "/hello-world")
   public String sayHello() {
-    return "Hello Word";
+    return "Hello Word " + teamCoach + " " + teamManager + " " + teamOwner;
   }
 
 }
