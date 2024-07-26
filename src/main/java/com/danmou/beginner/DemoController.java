4572318ecfeb2bbeacdf8ca51959298bbb5e3ce9
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-26 12:20:18 -0300

    bean lifecycle
    Examples of PostBuild and PreDesstroy annotations

--- src/main/java/com/danmou/beginner/DemoController.java
+++ src/main/java/com/danmou/beginner/DemoController.java
@@ -6,27 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.danmou.beginner.common.Coach;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class DemoController {
   private Coach myCoach;
-  private Coach anotherCoach;
 
   @Autowired
-  public DemoController(@Qualifier("tennisCoach") Coach coach, @Qualifier("tennisCoach") Coach anotherCoach) {
+  public DemoController(@Qualifier("cricketCoach") Coach coach) {
     this.myCoach = coach;
-    this.anotherCoach = anotherCoach;
   }
 
   @GetMapping("/daily-workout")
   public String getDailyWorkout() {
     return myCoach.getDailyWorkout();
-  }
-
-  @GetMapping("/check-scope")
-  public String checkCoachesInstances() {
-      return "Comparison is " + (myCoach == anotherCoach);
   }
 }
--- src/main/java/com/danmou/beginner/common/BaseballCoach.java
+++ src/main/java/com/danmou/beginner/common/BaseballCoach.java
@@ -2,8 +2,25 @@ package com.danmou.beginner.common;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 public class BaseballCoach implements Coach {
+
+  public BaseballCoach() {
+    System.out.println("In constructor " + getClass().getSimpleName());
+  }
+
+  @PostConstruct
+  public void start() {
+    System.out.println("Start up " + getClass().getSimpleName());
+  }
+
+  @PreDestroy
+  public void stop() {
+    System.out.println("Stop down " + getClass().getSimpleName());
+  }
 
   @Override
   public String getDailyWorkout() {
