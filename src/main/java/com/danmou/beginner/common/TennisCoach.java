6201d59a30f808945959a1ab9737ab3f6ef187c4
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-26 11:33:35 -0300

    lazy initialization
    Lazy Initialization can bet set up indivudially with annotations or global with properties.
    If set globally, no individual is needed. Code uses both for example purpose

--- src/main/java/com/danmou/beginner/common/TennisCoach.java
+++ src/main/java/com/danmou/beginner/common/TennisCoach.java
@@ -1,9 +1,15 @@
 package com.danmou.beginner.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class TennisCoach implements Coach {
+
+  public TennisCoach() {
+    System.out.println("In constructor " + getClass().getSimpleName());
+  }
 
   @Override
   public String getDailyWorkout() {
