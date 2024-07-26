aeed73656d44f7334a3eebedb8b2cb7bd98902d2
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-26 18:04:15 -0300

    reseting project
    Add postgres and starter-data-jpa dependency.
    Comment out started-web to turn it a Command Line App.
    Clean old classes.

--- src/main/java/com/danmou/beginner/BeginnerApplication.java
+++ src/main/java/com/danmou/beginner/BeginnerApplication.java
@@ -1,12 +1,21 @@
 package com.danmou.beginner;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = {"com.danmou.beginner", "com.danmou.util"})
+@SpringBootApplication()
 public class BeginnerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeginnerApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> { 
+			System.out.println("Hello World");
+		};
 	}
 }
