46804d6fe9b47808fc459af14e03677db0a997ae
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-31 15:08:15 -0300

    generic error handling

--- src/main/java/com/danmou/beginner/rest/StudentController.java
+++ src/main/java/com/danmou/beginner/rest/StudentController.java
@@ -54,4 +54,14 @@ public class StudentController {
 
     return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.NOT_FOUND);
   }
+
+  @ExceptionHandler
+  public ResponseEntity<StudentErrorResponse> handleException(Exception e){
+    StudentErrorResponse error = new StudentErrorResponse();
+    error.setStatus(HttpStatus.BAD_REQUEST.value());
+    error.setMessage(e.getMessage());
+    error.setTimestamp(System.currentTimeMillis());
+
+    return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.BAD_REQUEST);
+  }
 }
