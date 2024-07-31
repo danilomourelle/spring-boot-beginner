2c9bbe57359a60f39c95d13dfff561bdb4f48658
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-31 14:29:37 -0300

    path variable

--- src/main/java/com/danmou/beginner/rest/StudentController.java
+++ src/main/java/com/danmou/beginner/rest/StudentController.java
@@ -4,23 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.danmou.beginner.entity.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api")
 public class StudentController {
 
-  @GetMapping("/students")
-  public List<Student> getStudents() {
-    List<Student> students = new ArrayList<>();
+  private List<Student> students = new ArrayList<>();
 
+  @PostConstruct
+  public void loadData() {
     students.add(new Student("Poornima", "Patel"));
     students.add(new Student("Mario", "Rossi"));
     students.add(new Student("Mary", "Smith"));
-    
+  }
+
+  @GetMapping("/students")
+  public List<Student> getStudents() {
+
     return students;
+  }
+
+  @GetMapping("/students/{studentId}")
+  public Student getStudents(@PathVariable int studentId) {
+
+    return students.get(studentId);
   }
 }
