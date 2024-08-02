d3df8e8cb0af66bd797e358b0c79ae10b3fa9167
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-08-02 11:53:14 -0300

    reseting project

---
+++ src/main/java/com/danmou/beginner/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.danmou.beginner.service;
+
+import java.util.List;
+
+import com.danmou.beginner.entity.Employee;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+    Employee findById(int theId);
+    Employee save(Employee theEmployee);
+    void deleteById(int theId);
+}
