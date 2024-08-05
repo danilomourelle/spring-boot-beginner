f71fe08339ee70d0b7d5f31cf385a63ae7e6a16d
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-08-05 11:42:17 -0300

    reseting project

--- src/main/java/com/danmou/beginner/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.danmou.beginner.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.danmou.beginner.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
