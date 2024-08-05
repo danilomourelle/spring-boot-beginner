85a63894989c24f5be96cff4974639c6521e9c68
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-08-05 15:37:47 -0300

    replacing servlet by annotation to bind

--- src/main/java/com/danmou/beginner/controller/FormController.java
+++ src/main/java/com/danmou/beginner/controller/FormController.java
@@ -3,6 +3,7 @@ package com.danmou.beginner.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -23,6 +24,15 @@ public class FormController {
      String theName = request.getParameter("studentName");
      theName = theName.toUpperCase();
      String result = "Yo! " + theName;
+     model.addAttribute("message", result);
+     
+     return "process2";
+  }
+
+  @GetMapping("form-three")
+  public String someMethod(@RequestParam("studentName") String theName, Model model) {
+     theName = theName.toUpperCase();
+     String result = "Dude! " + theName;
      model.addAttribute("message", result);
      
      return "process2";
