cf39e3e455deeb8a23430772e9bfc2ce925dd72e
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-08-05 16:04:41 -0300

    using POST on form

--- src/main/java/com/danmou/beginner/controller/FormController.java
+++ src/main/java/com/danmou/beginner/controller/FormController.java
@@ -3,6 +3,7 @@ package com.danmou.beginner.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -29,7 +30,7 @@ public class FormController {
      return "process2";
   }
 
-  @GetMapping("form-three")
+  @PostMapping("form-three")
   public String someMethod(@RequestParam("studentName") String theName, Model model) {
      theName = theName.toUpperCase();
      String result = "Dude! " + theName;
