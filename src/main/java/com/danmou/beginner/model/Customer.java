2c4d3753f2683957754956651add2efbb54336d7
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-08-06 13:01:29 -0300

    min and max validation

--- src/main/java/com/danmou/beginner/model/Customer.java
+++ src/main/java/com/danmou/beginner/model/Customer.java
@@ -1,5 +1,7 @@
 package com.danmou.beginner.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,10 @@ public class Customer {
   @NotNull(message = "required")
   @Size(min = 2, message = "at least two character")
   private String lastName;
+
+  @Min(value = 0, message = "must be greater than or equal to 0")
+  @Max(value = 10, message = "must be less than or equal to 10")
+  private Integer freePasses;
 
   public Customer() {
 
@@ -28,5 +34,13 @@ public class Customer {
 
   public void setLastName(String lastName) {
     this.lastName = lastName;
+  }
+
+  public Integer getFreePasses() {
+    return freePasses;
+  }
+
+  public void setFreePasses(Integer freePasses) {
+    this.freePasses = freePasses;
   }
 }
