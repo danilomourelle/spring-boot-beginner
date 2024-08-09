b80af725475f21f8a23d5b58d38a542c201b86ed
Danilo Mourelle <danilomourelle@outlook.com>
2024-08-09 17:14:41 -0300

    setting security to use database user infos

--- src/main/java/com/danmou/beginner/security/DemoConfigSecurity.java
+++ src/main/java/com/danmou/beginner/security/DemoConfigSecurity.java
@@ -1,59 +1,42 @@
 package com.danmou.beginner.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoConfigSecurity {
 
-  @Bean
-  public InMemoryUserDetailsManager userDetailsManager() {
-    UserDetails john = User.builder()
-        .username("john")
-        .password("{noop}test123")
-        .roles("EMPLOYEE")
-        .build();
+    @Bean
+    UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
-    UserDetails mary = User.builder()
-        .username("mary")
-        .password("{noop}test123")
-        .roles("EMPLOYEE", "MANAGER")
-        .build();
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/authenticate")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/custom-logout")
+                        .logoutSuccessUrl("/login?logout_custom")
+                        .permitAll())
+                .exceptionHandling(configurer -> configurer
+                        .accessDeniedPage("/access-denied"));
 
-    UserDetails susan = User.builder()
-        .username("susan")
-        .password("{noop}test123")
-        .roles("EMPLOYEE", "ADMIN")
-        .build();
-
-    return new InMemoryUserDetailsManager(john, mary, susan);
-  }
-
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-        .authorizeHttpRequests(configurer -> configurer
-            .requestMatchers("/").hasRole("EMPLOYEE")
-            .requestMatchers("/leaders/**").hasRole("MANAGER")
-            .requestMatchers("/systems/**").hasRole("ADMIN")
-            .anyRequest()
-            .authenticated())
-        .formLogin(form -> form
-            .loginPage("/login")
-            .loginProcessingUrl("/authenticate")
-            .permitAll())
-        .logout(logout -> logout
-            .logoutUrl("/custom-logout")
-            .logoutSuccessUrl("/login?logout_custom")
-            .permitAll())
-        .exceptionHandling(configurer -> configurer
-            .accessDeniedPage("/access-denied"));
-
-    return http.build();
-  }
+        return http.build();
+    }
 }
