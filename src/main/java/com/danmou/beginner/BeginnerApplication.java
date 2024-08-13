3a7b6efab61be71b74b7c0ccd68487109a11278e
Danilo Mourelle <danilomourelle@outlook.com>
2024-08-13 12:24:57 -0300

    setting project to run command line

--- src/main/java/com/danmou/beginner/BeginnerApplication.java
+++ src/main/java/com/danmou/beginner/BeginnerApplication.java
@@ -1,12 +1,21 @@
 package com.danmou.beginner;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication()
 public class BeginnerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeginnerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
 	}
 }
